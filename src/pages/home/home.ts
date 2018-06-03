1cbfdf4c6ac7387b168b8d54109c5877662906ef
Charles Albert <charles.ccomp@gmail.com>
2018-06-02 22:32:11 -0300

    implemented app basic functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  posts: any
+  subReddit = "EarthPorn"
 
+  constructor(public navCtrl: NavController, public http: Http) {
+    this.http.get('https://www.reddit.com/r/'+this.subReddit+'/new/.json?limit=10').map(res => res.json()).subscribe(data => {
+      this.posts = data.data.children;
+    })
   }
 
 }
